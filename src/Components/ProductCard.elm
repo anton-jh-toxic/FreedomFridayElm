bf14bcf9a06861c5781005643ad202b19c3b7e48
Lucas Strand <lucas.strand@outlook.com>
2022-12-16 14:17:10 +0100

    card component strted

--- src/Components/ProductCard.elm
+++ src/Components/ProductCard.elm
@@ -1,3 +1,25 @@
 module Components.ProductCard exposing (..)
 
+import Bootstrap.Button as Button
+import Bootstrap.CDN as CDN
+import Bootstrap.Card as Card
+import Bootstrap.Card.Block as Block
+import Bootstrap.Grid as Grid
+import Bootstrap.Utilities.Spacing as Spacing
+import Html as H
+import Html.Attributes as A
 
+
+view model =
+    Card.config [ Card.attrs [ A.style "width" "20rem" ] ]
+        |> Card.header [ A.class "text-center" ]
+            [ H.img [ A.src "assets/images/elm-bootstrap.svg" ] []
+            , H.h3 [ Spacing.mt2 ] [ H.text "Custom Card Header" ]
+            ]
+        |> Card.block []
+            [ Block.titleH4 [] [ H.text "Card title" ]
+            , Block.text [] [ H.text "Some quick example text to build on the card title and make up the bulk of the card's content." ]
+            , Block.custom <|
+                Button.button [ Button.primary ] [ H.text "Go somewhere" ]
+            ]
+        |> Card.view
--- src/Main.elm
+++ src/Main.elm
@@ -1,18 +1,21 @@
-module Main exposing(..)
+module Main exposing (..)
 
-import Html as H exposing (Html)
-import Html.Events as E
-import Html.Attributes as A
 import Browser
+import Components.ProductCard as CardComponent
+import Html as H exposing (Html)
+import Html.Attributes as A
+import Html.Events as E
 
 
 type alias Model =
-    { number: Int
+    { number : Int
     }
+
 
 init : Model
 init =
     Model 0
+
 
 type Msg
     = DidStuff
@@ -25,21 +28,11 @@ update msg model =
             { model | number = model.number + 1 }
 
 
-
 view : Model -> Html Msg
-view model =
-    H.button
-        [ E.onClick DidStuff
-        , A.class "hurr"
-        , A.style "color" "black"
-        , A.type_ "button"
-        ]
-        [ H.text (String.fromInt model.number)
-        , H.p [] [ H.text "hurr" ]
-        ]
-
+view =
+    CardComponent.view
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view}
+    Browser.sandbox { init = init, update = update, view = view }
