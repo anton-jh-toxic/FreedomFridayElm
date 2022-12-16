ee24a4f0fc4da0066b1a6d66b47a94b79e69371c
Anton Johnsson Håkansson <anton.johnsson-hakansson@toxic.se>
2022-12-16 15:51:34 +0100

    api

--- src/Main.elm
+++ src/Main.elm
@@ -1,45 +1,80 @@
 module Main exposing (..)
 
 import Bootstrap.CDN as CDN
-import Bootstrap.Grid as Grid
+import Bootstrap.Grid as Grid exposing (row)
 import Browser
 import Components.ProductCard as CardComponent
 import Html as H exposing (Html)
 import Html.Attributes as A
 import Html.Events as E
+import Stuff.Api as Api exposing (Product)
 
 
-type alias Model =
-    { number : Int
+type Model
+    = Loading
+    | Failure
+    | Success ShopModel
+
+
+type alias ShopModel =
+    { products : List Product
     }
 
 
-init : Model
-init =
-    Model 0
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Loading
+    , Api.getAllProducts (Api.hideError >> GotStuff)
+    )
 
 
 type Msg
-    = DidStuff
+    = GotStuff (Result () (List Product))
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        DidStuff ->
-            { model | number = model.number + 1 }
+        GotStuff result ->
+            case result of
+                Result.Err _ ->
+                    ( Failure, Cmd.none )
+
+                Result.Ok res ->
+                    ( Success (ShopModel res), Cmd.none )
 
 
 view model =
     Grid.container []
         [ CDN.stylesheet -- creates an inline style node with the Bootstrap CSS
         , Grid.row []
-            [ Grid.col []
-                [ CardComponent.view () ]
-            ]
+            (viewProductList model)
         ]
+
+
+viewProductList : Model -> List (Grid.Column Msg)
+viewProductList model =
+    case model of
+        Loading ->
+            [ Grid.col [] [ H.text "LOADING..." ] ]
+
+        Failure ->
+            [ Grid.col [] [ H.text "Failed" ] ]
+
+        Success shop ->
+            List.map viewProduct shop.products
+
+
+viewProduct : Product -> Grid.Column Msg
+viewProduct product =
+    Grid.col [] [ CardComponent.view product ]
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
